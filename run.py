bc5916cfdf98ef21b7a0828b98b57ccd72012724
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-18 12:24:32 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,30 @@
+#數字
+3456
+3.14
+
+#字串
+"Hello World"
+
+#布林值
+True
+False
+
+#列表List 有順序、可動[]
+[3,4,5]
+["Hi","Python"]
+
+#列表Tuple 有順序、不可動()
+(3,4,5)
+("Hi","Python")
+
 #集合
+{4,5,6}
+{"Hi","Python"}
+
+#字典Dictionary {:}
+{"apple":"蘋果","banana":"香蕉"}
+
+#變數
 i={4,5,6}
-print(i)   #{4,5,6}
-print(5 in i)   #True
+print(x)      #{4,5,6}
+
